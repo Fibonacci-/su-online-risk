da63518471515c9fe6c750ff4145167498c569bf
toshiro <kubota@susqu.edu>
2014-02-04 11:44:10 -0500

    added map project

---
+++ Kubota/Example01/MapStuff/Continent.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RiskMap
+{
+    [Serializable]
+    class Continent
+    {
+        // Variables.
+        protected string name;
+        protected int unitBonus;
+        protected List<Territory> territories;
+        protected int id;
+
+        // Constructor.
+        private Continent(string name, int unit, List<Territory> ter)
+        {
+            this.name = name;
+            this.unitBonus = unit;
+            this.territories = ter;
+        }
+
+        // Getting name.
+        public string getName() { return (name); }
+
+        // Getting unit bonus.
+        public int getBonus() { return (unitBonus); }
+
+        // Checking to see if the whole continent has been captured by a single player.
+        public bool isCaptured()
+        {
+            // Will work on this later.
+            return (false);
+        }
+
+        // Getting territory list.
+        public List<Territory> getTerritories() { return (territories); }
+
+        // Checkking for a specific territory.
+        public bool isTerritory(Territory czech) { return (territories.Contains(czech)); }
+    }
+}
---
+++ Kubota/Example01/MapStuff/Map.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RiskMap
+{
+    [Serializable]
+    class Map
+    {
+        // Variables.
+        protected List<Continent> continents;
+        protected List<Territory> territories;
+        protected string fileName;
+        //protected imageFile;
+
+        // Constructor.
+        private Map(string file)
+        {
+            // Note:    File string does not include extensions. This is so we only need one
+            //          variable, and also because the filename of the image and code are
+            //          going to be the same.
+
+            this.fileName = file;
+            // Will be done next week.
+        }
+
+        // Returning a specific continent.
+        public Continent getOneContinent(int i) { return continents.ElementAt(i); }
+
+        // Returning a specific territory.
+        public Territory getOneTerritory(int i) { return territories.ElementAt(i); }
+
+        // Returning all continents.
+        public List<Continent> getAllContinents() { return continents; }
+
+        // Returning all territories.
+        public List<Territory> getAllTerritories() { return territories; }
+
+        // Saving the map.
+        protected void saveMap()
+        {
+            // Will work on this later.
+        }
+
+        // Loading the necessary files and deciphering the mapcode.
+        protected bool loadMap()
+        {
+            // Loading files.
+            if (!File.Exists(fileName + ".txt") || !File.Exists(fileName + ".png")) return false;
+            else
+            {
+                FileStream fileRead = File.OpenRead(fileName + ".txt");
+                // Load image file later.
+            }
+
+            // Deserializing map code.
+
+            // Ending.
+            return true;
+        }
+    }
+}
