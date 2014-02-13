c47eb886e6fcc033b3c19958124e4fda153dee25
toshiro <kubota@susqu.edu>
2014-02-13 11:24:55 -0500

    added ClientDemo - mock risk games.
    moved utility classes (Map, Territory, Continent, Player, Message, State, ReinforcementCard, etc.) to SURiskLibrary.
    changed namespace SUOnlineRisk for these two projects.

--- Kubota/Example01/StateMachines/Server.cs
+++ Kubota/Example01/StateMachines/Server.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
 
-namespace StateMachines
+namespace SUOnlineRisk
 {
     class Server
     {
--- Kubota/Example01/StateMachines/State.cs
+++ Kubota/Example01/StateMachines/State.cs
@@ -4,63 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace StateMachines
+namespace SUOnlineRisk
 {
-    enum MainState { Start, Initialize, Distribute, Collect, Reinforce, Attack, Fortify, Over};
-    /*class State
-    {
-
-        public MainState mainState
-        {
-            get;
-            set;
-        }
-        public State()
-        {
-            mainState = MainState.Start;
-        }
-        public void Next()
-        {
-            lock(this)
-            {
-                switch (mainState)
-                {
-                    case MainState.Start:
-                        mainState = MainState.Initialize;
-                        break;
-                    case MainState.Initialize:
-                        mainState = MainState.Distribute;
-                        break;
-                    case MainState.Distribute:
-                        mainState = MainState.Collect;
-                        break;
-                    case MainState.Collect:
-                        mainState = MainState.Reinforce;
-                        break;
-                    case MainState.Reinforce:
-                        mainState = MainState.Attack;
-                        break;
-                    case MainState.Attack:
-                        mainState = MainState.Fortify;
-                        break;
-                    case MainState.Fortify:
-                        mainState = MainState.Collect;
-                        Activate(true);
-                        break;
-                    case MainState.Over:
-                        mainState = MainState.Over;
-                        Activate(true);
-                        break;
-                }
-            }
-        }
-        public void GameOver()
-        {
-            lock(this)
-            {
-                mainState = MainState.Over;
-                Activate(true);
-            }
-        }
-    }*/
+    enum MainState { Start, Initialize, Distribute, Collect, Reinforce, Attack, Fortify, Roll, Update, Over};
 }
