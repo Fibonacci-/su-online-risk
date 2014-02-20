2069b7fbe7453848f605669cfb405834cf9c4a93
lewisjg <lewisjg@susqu.edu>
2014-02-20 10:58:13 -0500

    Initial commit for initialization phase of GUI

---
+++ Lewis/LewisGUI/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using SUOnlineRisk;
+
+namespace LewisGUIExample
+{
+    public partial class Form1 : Form
+    {
+        Map map;
+        string mapfilename = @"..\..\..\MapStuff\SimpleRisk.map";
+        public Form1()
+        {
+            InitializeComponent();
+            if(File.Exists(mapfilename) == false)
+            {
+                MessageBox.Show(mapfilename + " does not exist.");
+                Application.Exit();
+            }
+            map = Map.loadMap(mapfilename);
+            this.pictureBox1.Image = map.getBitmap();
+        }
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            Point p = new Point();
+            p.X = e.Location.X * map.getBitmap().Width / pictureBox1.Width;
+            p.Y = e.Location.Y * map.getBitmap().Height / pictureBox1.Height;
+            MessageBox.Show("Clicked at(" + p.X + ", " + p.Y + ")");
+        }
+    }
+}
