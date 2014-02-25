8e8d2b7a1b533c423962042cd50ffea489c7a214
toshiro <kubota@susqu.edu>
2014-02-25 12:25:30 -0500

    used .NET 4.0 to compile SUOnlineLibrary for compatibility.

--- Kubota/Example01/InitializationGUI/Form1.Designer.cs
+++ Kubota/Example01/InitializationGUI/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace LewisGUIExample
+﻿namespace InitializationGUI
 {
     partial class Form1
     {
@@ -35,13 +35,12 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.pictureBox1.Location = new System.Drawing.Point(16, 15);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(755, 452);
+            this.pictureBox1.Size = new System.Drawing.Size(566, 367);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -50,23 +49,21 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(39, 38);
+            this.label1.Location = new System.Drawing.Point(475, 344);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.Size = new System.Drawing.Size(37, 13);
             this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.label1.Text = "Units: ";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(787, 481);
+            this.ClientSize = new System.Drawing.Size(590, 391);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
--- Kubota/Example01/LewisGUIExample/Form1.cs
+++ Kubota/Example01/LewisGUIExample/Form1.cs
@@ -16,6 +16,7 @@ namespace LewisGUIExample
     {
         Map map;
         string mapfilename = @"..\..\..\MapStuff\SimpleRisk.map";
+        Dictionary<Territory,Label> territoryLabels;
         public Form1()
         {
             InitializeComponent();
@@ -26,13 +27,62 @@ namespace LewisGUIExample
             }
             map = Map.loadMap(mapfilename);
             this.pictureBox1.Image = map.getBitmap();
+            this.Controls.Remove(this.pictureBox1);
+            territoryLabels = new Dictionary<Territory,Label>();
+            foreach(Territory t in map.getAllTerritories())
+            {
+                Label label = new Label();
+                label.Text = t.getOwner() + ": " + t.numArmies;
+                //label.Anchor = AnchorStyles.Bottom | AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+                label.Name = t.getOwner();
+                label.Size = new System.Drawing.Size(100, 20);
+                this.Controls.Add(label);
+                territoryLabels[t] = label;
+            }
+            relocateLabel();
+            this.Controls.Add(this.pictureBox1);
         }
         private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
         {
             Point p = new Point();
             p.X = e.Location.X * map.getBitmap().Width / pictureBox1.Width;
             p.Y = e.Location.Y * map.getBitmap().Height / pictureBox1.Height;
-            MessageBox.Show("Clicked at(" + p.X + ", " + p.Y + ")");
+            //MessageBox.Show("Clicked at(" + p.X + ", " + p.Y + ")");
+            float rad = 10;
+            foreach (Territory t in map.getAllTerritories())
+            {
+                float dx = p.X - t.returnX();
+                float dy = p.Y - t.returnY();
+                if (Math.Abs(dx) < rad && Math.Abs(dy) < rad)
+                {
+                    if (e.Button == System.Windows.Forms.MouseButtons.Left)
+                    {
+                        MessageBox.Show("Attacking from territory " + t.getName() + " owned by " + t.getOwner()+ ".");
+                    }
+                    else if(e.Button == System.Windows.Forms.MouseButtons.Right)
+                    {
+                        MessageBox.Show("Attacking to territory " + t.getName() + " owned by " + t.getOwner() + ".");
+                    }
+                    break;
+                }
+            }
+            
+        }
+
+        void relocateLabel()
+        {
+            foreach(Territory t in territoryLabels.Keys)
+            {
+                int x = t.returnX() * pictureBox1.Width / map.getBitmap().Width;
+                int y = t.returnY() * pictureBox1.Height / map.getBitmap().Height;
+                territoryLabels[t].Location = new Point(x, y + 40);
+            }
+
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            relocateLabel();
         }
     }
 }
