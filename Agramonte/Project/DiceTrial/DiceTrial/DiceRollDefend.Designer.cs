6c766c29e10fdc3c810dc9a690b2d7b8dbe8af7b
agramonte <agramonte@susqu.edu>
2014-03-05 23:33:10 -0500

    Updated version of Dice Roller. Beginning of Attack Phase

---
+++ Agramonte/Project/DiceTrial/DiceTrial/DiceRollDefend.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace LewisGUIExample
+{
+    partial class DiceRollDefend
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.labelDiceResult = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.buttonRoll1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(148, 47);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(84, 78);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox2.Location = new System.Drawing.Point(148, 182);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(84, 78);
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // labelDiceResult
+            // 
+            this.labelDiceResult.AutoSize = true;
+            this.labelDiceResult.Location = new System.Drawing.Point(77, 330);
+            this.labelDiceResult.Name = "labelDiceResult";
+            this.labelDiceResult.Size = new System.Drawing.Size(203, 13);
+            this.labelDiceResult.TabIndex = 6;
+            this.labelDiceResult.Text = "Choose the number of die you wish to roll.";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(111, 346);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 7;
+            // 
+            // buttonRoll1
+            // 
+            this.buttonRoll1.Location = new System.Drawing.Point(135, 380);
+            this.buttonRoll1.Name = "buttonRoll1";
+            this.buttonRoll1.Size = new System.Drawing.Size(75, 23);
+            this.buttonRoll1.TabIndex = 8;
+            this.buttonRoll1.Text = "Roll Die";
+            this.buttonRoll1.UseVisualStyleBackColor = true;
+            this.buttonRoll1.Click += new System.EventHandler(this.buttonRoll1_Click);
+            // 
+            // DiceRollDefend
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(358, 465);
+            this.Controls.Add(this.buttonRoll1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.labelDiceResult);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "DiceRollDefend";
+            this.Text = "DiceRollDefend";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label labelDiceResult;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button buttonRoll1;
+    }
+}
---
+++ Agramonte/Project/DiceTrial/DiceTrial/MainForm.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+using SUOnlineRisk;
+
+namespace LewisGUIExample
+{
+    public partial class MainForm : Form
+    {
+        Map map;
+        string mapfilename = @"..\..\..\MapStuff\SimpleRisk.map";
+        bool selectedTerr = false;
+        Territory attackFrom;
+        public MainForm()
+        {
+            InitializeComponent();
+            if (File.Exists(mapfilename) == false)
+            {
+                MessageBox.Show(mapfilename + " does not exist.");
+                Application.Exit();
+            }
+            map = Map.loadMap(mapfilename);
+            this.pictureBox1.Image = map.getBitmap();
+            map.addPlayer(new Player("Attacker", Color.Blue));
+            map.addPlayer(new Player("Defender", Color.Red));
+            for (int i = 0; i < map.getAllTerritories().Count; i++)
+            {
+                if (i <= (map.getAllTerritories().Count / 2))
+                {
+                    map.getOneTerritory(i).setOwner("Attacker");
+                }
+                else
+                {
+                    map.getOneTerritory(i).setOwner("Defender");
+                }
+            }
+        }
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            Point p = new Point();
+            p.X = e.Location.X * map.getBitmap().Width / pictureBox1.Width;
+            p.Y = e.Location.Y * map.getBitmap().Height / pictureBox1.Height;
+            //MessageBox.Show("Clicked at(" + p.X + ", " + p.Y + ")");
+            if (selectedTerr == false)
+            {
+                foreach (Territory t in map.getAllTerritories())
+                {
+                    if (((p.X > t.returnX() - 10) && (p.X < t.returnX() + 10)) && ((p.Y > t.returnY() - 10) && (p.Y < t.returnY() + 10)))
+                    {
+                        foreach (Player thisp in map.getAllPlayers())
+                        {
+                            string name = thisp.nickname;
+                            if (t.getOwner() == name)
+                            {
+                                if (MessageBox.Show("You selected " + t.getName() + ". Is this correct?",
+                                    "Yes", MessageBoxButtons.YesNo) == System.Windows.Forms.DialogResult.Yes)
+                                {
+                                    MessageBox.Show("Please select an adjacent enemy territory to attack.");
+                                    selectedTerr = true;
+                                    attackFrom = t;
+                                }
+                            }
+                        }
+                        break;
+
+                    }
+                }
+            }
+            else
+            {
+                foreach (Territory t in attackFrom.returnNeighbors())
+                {
+                    if (((p.X > t.returnX() - 10) && (p.X < t.returnX() + 10)) && ((p.Y > t.returnY() - 10) && (p.Y < t.returnY() + 10)))
+                    {
+                        if (t.getOwner() != attackFrom.getOwner())
+                        {
+                            if (MessageBox.Show("You selected " + t.getName() + ". Is this correct?",
+                                "Yes", MessageBoxButtons.YesNo) == System.Windows.Forms.DialogResult.Yes)
+                            {
+                                //opens new form for attacking
+                                selectedTerr = false;
+                                DiceForm Attack = new DiceForm();
+                                Attack.setOptionAttack();
+                                Attack.ShowDialog();
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("You cannot attack this territory. You own this territory.");
+                            //pop-up an error message.
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+
+        //attack button
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DiceForm f2 = new DiceForm();
+            f2.setOptionAttack();
+            f2.ShowDialog();
+        }
+
+        //defend button
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DiceRollDefend f3 = new DiceRollDefend();
+            f3.setOptionDefend();
+            f3.ShowDialog();
+        }
+
+    }
+
+}
