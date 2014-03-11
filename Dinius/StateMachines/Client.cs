42ba64d2d3c4e6a4cfedd09b5eb707ace887c72f
phildinius <dinius@susqu.edu>
2014-03-11 10:08:53 -0400

    Yes

---
+++ Dinius/StateMachines/Client.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace SUOnlineRisk
+{
+    class Client
+    {
+        Message acknowledge;
+        Message request;
+        Server server;
+        bool bAck;
+        bool bPending;
+        public bool bRunning
+        {
+            get;
+            private set;
+        }
+        public Player player
+        {
+            get;
+            set;
+        }
+        public Client(Server server)
+        {
+            this.server = server;
+            player = null;
+            bAck = false;
+            bPending = false;
+        }
+        //from the server
+        public void Request(Message message)
+        {
+            lock (this)
+            {
+                this.request = message;
+                bPending = true;
+            }
+        }
+        //from the player
+        public void Accept(Message message)
+        {
+            lock (this)
+            {
+                bAck = true;
+                acknowledge = message;
+            }
+        }
+        public void Run()
+        {
+            if (player == null)
+            {
+                Console.Error.WriteLine("There is no player.");
+                return;
+            }
+            if (server == null)
+            {
+                Console.Error.WriteLine("There is no server.");
+                return;
+            }
+            bRunning = true;
+            while (true)
+            {
+                while(bPending == false)
+                {
+                    Thread.Sleep(100);
+                }
+                lock (this)
+                {
+                    bPending = false;
+                    player.Request(request);
+                }
+                while(bAck == false)
+                {
+                    Thread.Sleep(100);
+                }
+                lock (this)
+                {
+                    bAck = false;
+                    server.Accept(this, acknowledge);
+                }
+                if(player.bRunning == false)
+                {
+                    break;
+                }
+            }
+            bRunning = false;
+        }
+    }
+}
