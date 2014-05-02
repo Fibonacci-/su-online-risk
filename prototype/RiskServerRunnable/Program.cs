314a7f22152f0aa9ed7dd43a85d94803c636258e
helwig <helwigdev@gmail.com>
2014-05-02 10:39:13 -0400

    Added server executable

---
+++ prototype/RiskServerRunnable/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace RiskServerRunnable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost host = new ServiceHost(typeof(WCFRiskServer.RiskServer)))
+            {
+                host.Open();
+                Console.WriteLine("Enter 'stop' to terminate server");
+                while (true)
+                {
+                    string s = Console.ReadLine();
+                    if (String.Compare(s, "stop", true) == 0)
+                    {
+                        break;
+                    }
+                    Console.WriteLine("Didn't catch that");
+                }
+            }
+        }
+    }
+}
